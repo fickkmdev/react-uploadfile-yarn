00c4cccfb9cde9c651009f8ad9ca1f824f615e63
fickkmdev <fickkm.dev@gmail.com>
2022-01-31 23:49:20 +0700

    new-pj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [newName, setNewName] = useState("")
+  const [newPhoneNumber, setNewPhoneNumber] = useState("")
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map(
+        (doc) => (
+          { ...doc.data(), id: doc.id }
+        )
+      ))
+    };
+
+    getUsers();
+  }, []);
+
+  const addNewUser = async()=>{
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder='name'
+        type={'text'}
+        onChange={(event)=>{
+          setNewName(event.target.value)
+        }}
+      />
+      {" "}
+      <input
+        placeholder='phone number'
+        type={'text'}
+        onChange={(event)=>{
+          setNewPhoneNumber(event.target.value)
+        }}
+      />
+      {" "}
+      <button
+        onClick={addNewUser}>
+        Add
+      </button>
+      {users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <p>Name: {user.name}</p>
+            <p>Phone number: {user.p_number}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
